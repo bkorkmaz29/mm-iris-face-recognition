07ee97522cae6df218d0e4fc3f73f74feb6feb87
Baran Korkmaz <68343982+bkorkmaz29@users.noreply.github.com>
2023-01-23 21:34:05 +0100

    new ui

--- controllers.py
+++ controllers.py
@@ -1,24 +1,37 @@
 import os
 from glob import glob
-
-from functions.multimodal.fusion_score import get_fusion_scores
+from cv2 import imencode
+from functions.multimodal.fusion_score import get_fusion_scores, load_image_file
 from functions.face.face_add import face_add
 from functions.iris.iris_add import iris_add
+from PIL import Image
+import numpy as np
 
 DATA_DIR = "data/db/"
-
+ 
 def on_add(face_dir, iris_dir):
     files = glob("data/db/*")
     idx = str(len(files) + 1)
     save_dir = DATA_DIR + idx
-
+ 
     if not os.path.exists(save_dir):
         print("makedirs", save_dir)
         os.makedirs(save_dir)
-
+ 
     face_add(face_dir, idx, DATA_DIR)
     iris_add(iris_dir, idx, DATA_DIR)
-
-
+ 
+ 
 def on_rec(face_dir, iris_dir):
-    return get_fusion_scores(face_dir, iris_dir, DATA_DIR)
+    scores, index_max = get_fusion_scores(face_dir, iris_dir, DATA_DIR)
+    return scores, index_max
+ 
+def get_image(dir):
+    im = Image.open(dir)
+    im.thumbnail((320,243), Image.Resampling.LANCZOS)
+    
+    im = im.convert('RGB')
+    image = np.array(im)
+    imgbytes = imencode('.ppm', image)[1].tobytes()
+
+    return imgbytes
--- ui.py
+++ ui.py
@@ -2,68 +2,76 @@ import PySimpleGUI as sg
 import numpy as np
 import os
 
-from controllers import on_rec, on_add
 
-
+from controllers import on_rec, on_add, get_image
+ 
+ 
 def main():
-
-    sg.theme('SandyBeach')
-
-    main_layout = [
-       [sg.Image(filename='cover.png', key='image')]
-    ]
-
-
+ 
+    sg.theme('BlueMono')
+ 
     rec_layout = [
-        [sg.Text('Recognize', size=(40, 1), justification='center', font='Helvetica 20')],
-        [sg.Text('Face File'), sg.In(size=(25,1), enable_events=True ,key='-FACEFILE-'), sg.FileBrowse()],
-        [sg.Text('Iris File'), sg.In(size=(25,1), enable_events=True ,key='-IRISFILE-'), sg.FileBrowse()],
+        [sg.Text('Recognize', size=(40, 1),
+                 justification='center', font='Helvetica 20')],
+        [sg.Text('Face File', background_color='Ivory'), sg.In(
+            size=(25, 1), enable_events=True, key='-FACEREC-'), sg.FileBrowse()],
+        [sg.Text('Iris File', background_color='Ivory'), sg.In(
+            size=(25, 1), enable_events=True, key='-IRISREC-'), sg.FileBrowse()],
         [sg.Button('Search')]
     ]
-
-
+ 
     add_layout = [
-        [sg.Text('Add User', size=(40, 1), justification='center', font='Helvetica 20')],
-        [sg.Text('Name'), sg.InputText()],
-        [sg.Text('Face File'), sg.In(size=(25,1), enable_events=True ,key='-FACEFILE2-'), sg.FileBrowse()],
-        [sg.Text('Iris File'), sg.In(size=(25,1), enable_events=True ,key='-IRISFILE2-'), sg.FileBrowse()],
+        [sg.Text('Add User', size=(40, 1),
+                 justification='center', font='Helvetica 20')],
+        [sg.Text('Name', background_color='Ivory'), sg.InputText(key='-NAME-')],
+        [sg.Text('Face File', background_color='Ivory'), sg.In(
+            size=(25, 1), enable_events=True, key='-FACEADD-'), sg.FileBrowse()],
+        [sg.Text('Iris File', background_color='Ivory'), sg.In(
+            size=(25, 1), enable_events=True, key='-IRISADD-'), sg.FileBrowse()],
         [sg.Button('Add')]
     ]
-
-    
-    layout = [
-        [sg.Button('Recognize'), sg.Button('Add User'), sg.Button('Exit')],
-        [sg.Column(rec_layout, visible=False, key='rec'), sg.Column(add_layout, visible=False, key='add'), sg.Column(main_layout, visible=True, key='main')]
-    ]
-
-    window = sg.Window('app', layout, location=(300, 300), size=(900, 700))
  
-    input_layout = [
-        [sg.Text('Name', size =(15, 1)), sg.InputText()],
-        [sg.Submit(), sg.Cancel()]
-    ]
-    
-
+    tabgrp = [
+        [sg.TabGroup([[
+            sg.Button('Exit'),
+            sg.Tab('Recognition', rec_layout, title_color='Red', border_width=10, background_color='Ivory', tooltip='Recognition',
+                   element_justification='center'),
+            sg.Tab('Add New User', add_layout, title_color='Blue', background_color='Ivory', element_justification='center')]], tab_location='centertop',
+            border_width=5, tab_background_color='Gray', selected_background_color='Ivory')
+ 
+         ],
+        [sg.Frame('',[[
+            sg.Text('No Results', font='Helvetica 20', key='-TEXT-')],
+        [sg.Image(filename='', key='-FACEIMAGE-'),
+         sg.Image(filename='', key='-IRISIMAGE-')]], key='-IMAGEFRAME', border_width=5)
+    ]]
+ 
+    window = sg.Window('app', tabgrp, element_justification='center',
+                       location=(0, 0), size=(1200, 600))
+    face_elem = window['-FACEIMAGE-']
+    iris_elem = window['-IRISIMAGE-']
+    text_elem = window['-TEXT-']
     while True:
         event, values = window.read(timeout=20)
         if event == 'Exit' or event == sg.WIN_CLOSED:
             return
-        
-        elif event == 'Recognize':
-            window['main'].update(visible=False)
-            window['rec'].update(visible=True) 
-            window['add'].update(visible=False)        
-     
+ 
         elif event == 'Search':
-                on_rec(values["-FACEFILE-"], values["-IRISFILE-"]) 
-        
-
-                                     
-        elif event == 'Add User':
-            window['main'].update(visible=False)
-            window['rec'].update(visible=False) 
-            window['add'].update(visible=True)
-
+            face_img = get_image(values["-FACEREC-"])
+            iris_img = get_image(values["-IRISREC-"])
+            face_elem.update(data=face_img)
+            iris_elem.update(data=iris_img)
+            scores, result = on_rec(values["-FACEREC-"], values["-IRISREC-"])
+            res = str(result)
+            text_elem.update(res)
+            
         elif event == 'Add':
-            on_add(values["-FACEFILE2-"], values["-IRISFILE2-"]) 
+            face_img = get_image(values["-FACEADD-"])
+            iris_img = get_image(values["-IRISADD-"])
+            face_elem.update(data=face_img)
+            iris_elem.update(data=iris_img)
+            text_elem.update(values['-NAME-'])
+            on_add(values["-FACEADD-"], values["-IRISADD-"])
+ 
+ 
 main()
